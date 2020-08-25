6d8e9ae782e53d19a2f8fcffed26f2b8087d197f
Kendyl Hull <hullkendyl@gmail.com>
2020-08-25 10:50:08 -0400

    created Employee class & added fields

---
+++ src/main/java/Entities/Employee.java
@@ -0,0 +1,21 @@
+package Entities;
+
+public class Employee {
+
+    String firstName;
+    String lastName;
+    String phoneNumber;
+    String emailAddress;
+    String department;
+    String positionTitle;
+    String favoriteThings;
+    String importantNotes;
+    String startDate;
+    String endDate;
+    String threeMonthAnniversaryDate;
+    String sixMonthAnniversaryDate;
+    String oneYearAnniversaryDate;
+
+    
+
+}
