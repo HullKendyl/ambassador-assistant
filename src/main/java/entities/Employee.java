b1bc426c8e2cc89d3f631ea32970710d3769340e
Joe Grace <joeggrace@gmail.com>
2020-08-25 11:13:17 -0400

    finished employee POJO

--- src/main/java/entities/Employee.java
+++ src/main/java/entities/Employee.java
@@ -2,20 +2,76 @@ package entities;
 
 public class Employee {
 
-    String firstName;
-    String lastName;
-    String phoneNumber;
-    String emailAddress;
-    String department;
-    String positionTitle;
-    String favoriteThings;
-    String importantNotes;
-    String startDate;
-    String endDate;
-    String threeMonthAnniversaryDate;
-    String sixMonthAnniversaryDate;
-    String oneYearAnniversaryDate;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
+    private String department;
+    private String positionTitle;
+    private String favoriteThings;
+    private String importantNotes;
+    private String startDate;
+    private String endDate;
+    private String threeMonthAnniversaryDate;
+    private String sixMonthAnniversaryDate;
+    private String oneYearAnniversaryDate;
 
+    public Employee(String firstName, String lastName, String department, String positionTitle) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.positionTitle = positionTitle;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public String getFavoriteThings() {
+        return favoriteThings;
+    }
+
+    public String getImportantNotes() {
+        return importantNotes;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public String getThreeMonthAnniversaryDate() {
+        return threeMonthAnniversaryDate;
+    }
+
+    public String getSixMonthAnniversaryDate() {
+        return sixMonthAnniversaryDate;
+    }
+
+    public String getOneYearAnniversaryDate() {
+        return oneYearAnniversaryDate;
+    }
 }
